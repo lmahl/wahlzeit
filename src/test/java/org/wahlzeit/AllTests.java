4cc558edf132985836dad1db6ec2f853669378f2
fu85sade <wa-eBe9k>
2018-11-04 15:47:25 +0100

    Add AllTests Suite and adapt build.gradle to execute it in test-task

---
+++ src/test/java/org/wahlzeit/AllTests.java
@@ -0,0 +1,30 @@
+/**
+ * EmailServiceTestSuite
+ *
+ * Version: 1.0
+ *
+ * Date: 04.11.2018
+ *
+ * License: AGPLv3
+ */
+package org.wahlzeit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.wahlzeit.handlers.TellFriendTest;
+import org.wahlzeit.model.*;
+import org.wahlzeit.model.persistence.AbstractAdapterTest;
+import org.wahlzeit.model.persistence.DatastoreAdapterTest;
+import org.wahlzeit.services.EmailServiceTestSuite;
+import org.wahlzeit.services.LogBuilderTest;
+import org.wahlzeit.utils.StringUtilTest;
+import org.wahlzeit.utils.VersionTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({TellFriendTest.class, DatastoreAdapterTest.class,
+        AccessRightsTest.class, CoordinateTest.class, FlagReasonTest.class, GenderTest.class, GuestTest.class,
+        LocationTest.class, PhotoFilterTest.class, TagsTest.class, UserStatusTest.class, ValueTest.class,
+        EmailServiceTestSuite.class, LogBuilderTest.class, StringUtilTest.class, VersionTest.class})
+public class AllTests {
+
+}
--- src/test/java/org/wahlzeit/services/EmailServiceTestSuite.java
+++ src/test/java/org/wahlzeit/services/EmailServiceTestSuite.java
@@ -1,3 +1,12 @@
+/**
+ * EmailServiceTestSuite
+ *
+ * Version: 1.0
+ *
+ * Date: 04.11.2018
+ *
+ * License: AGPLv3
+ */
 package org.wahlzeit.services;
 
 import org.junit.runner.RunWith;
@@ -6,6 +15,6 @@ import org.wahlzeit.services.mailing.EmailServiceTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({EmailAddressTest.class, EmailServiceTest.class})
-public class EmailService {
+public class EmailServiceTestSuite {
 
 }
